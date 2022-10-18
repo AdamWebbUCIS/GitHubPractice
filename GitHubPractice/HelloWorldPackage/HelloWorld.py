d8b39ea1efcd1c44a3dc1ee798921b1fb9c375d9
adamw <adamw@DESKTOP-E1R2I6A.wifi.uc.edu>
2022-10-18 08:50:03 -0400

    Added print

--- GitHubPractice/HelloWorldPackage/HelloWorld.py
+++ GitHubPractice/HelloWorldPackage/HelloWorld.py
@@ -1,3 +1,10 @@
 #GitHub practice code
 
 print("Hello World")
+print("Indiana Basketball is the best")
+#Go to window, view, other, then type in git and click GitStaging, you will bring up the git page
+
+#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
+
+
+
